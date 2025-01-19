5b088e1431c80d196ee9f92f4fb7c06dcd7ee6f8
Enm32 <mureithieugene739@gmail.com>
2025-01-19 12:23:25 +0300

    extra

---
+++ app/src/main/java/com/example/progresscheckerforcbc/enm.java
@@ -0,0 +1,20 @@
+package com.example.progresscheckerforcbc;
+
+public class enm {
+    private String nm;
+
+    public enm() {
+    }
+
+    public enm(String nm) {
+        this.nm = nm;
+    }
+
+    public String getNm() {
+        return nm;
+    }
+
+    public void setNm(String nm) {
+        this.nm = nm;
+    }
+}
