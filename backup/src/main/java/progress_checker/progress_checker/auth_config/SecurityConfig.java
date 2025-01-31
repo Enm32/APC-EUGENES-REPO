4a1a41c520c49f61470c9fa74a13965fd6bf4778
Enm32 <mureithieugene739@gmail.com>
2025-01-31 12:16:04 +0300

    docker items

--- backup/src/main/java/progress_checker/progress_checker/auth_config/SecurityConfig.java
+++ backup/src/main/java/progress_checker/progress_checker/auth_config/SecurityConfig.java
@@ -1,9 +1,6 @@
 package progress_checker.progress_checker.auth_config;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,8 +10,6 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
--- backup/src/main/java/progress_checker/progress_checker/auth_config/UserDetailsServiceImpl.java
+++ backup/src/main/java/progress_checker/progress_checker/auth_config/UserDetailsServiceImpl.java
@@ -1,11 +1,8 @@
 package progress_checker.progress_checker.auth_config;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 
