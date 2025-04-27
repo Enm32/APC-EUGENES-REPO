467b422430a3ea93d40ec3400f72c3406227c9b5
Enm32 <mureithieugene739@gmail.com>
2025-04-27 13:12:29 +0300

    authenticate upgrade student endpoint

--- backup/src/main/java/progress_checker/progress_checker/auth_config/SecurityConfig.java
+++ backup/src/main/java/progress_checker/progress_checker/auth_config/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
                   //    })
                  authorizeHttpRequests(auth -> {
               auth
-                      .requestMatchers("/auth/get_token","/addStudent","/addTeacher","/send_otp","/notification","/verify_otp","/new_grade","/addsubject","/teachers/auth/get_token","/getallStudents","upgradeStudent/{id1}/{id2}").permitAll()
+                      .requestMatchers("/auth/get_token","/addStudent","/addTeacher","/send_otp","/notification","/verify_otp","/new_grade","/addsubject","/teachers/auth/get_token","/getallStudents").permitAll()
                       .anyRequest().authenticated();
           })
                   .sessionManagement(httpSecuritySessionManagementConfigurer -> {
